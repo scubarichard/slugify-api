7731234adc6c17e785a53883b3e18e2449dc84d6
scubarichard <richard@1altx.com>
2025-07-20 10:00:47 -0500

    Update slugify.js

--- api/slugify.js
+++ api/slugify.js
@@ -1,8 +1,12 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
   const { title } = req.body;
 
   if (!title) {
-    return res.status(400).json({ error: "Missing 'title'" });
+    return res.status(400).json({ error: 'Missing title field' });
   }
 
   const slug = title
